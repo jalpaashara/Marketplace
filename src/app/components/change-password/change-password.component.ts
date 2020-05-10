a40123f43ab31fab5a95c298823d11fa0cae566a
ashar <ashara.jalpa@gmail.com>
2020-05-09 22:37:50 -0700

    update user profile

---
+++ src/app/components/change-password/change-password.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import {AuthService} from "../../services/auth/auth.service";
+
+@Component({
+  selector: 'app-change-password',
+  templateUrl: './change-password.component.html',
+  styleUrls: ['./change-password.component.css']
+})
+export class ChangePasswordComponent implements OnInit {
+
+  constructor(public authService: AuthService) { }
+
+  ngOnInit(): void {
+  }
+
+  ChangePassword(value: string) {
+    this.authService.ChangePassword(value);
+    this.authService.SignOut();
+  }
+}
