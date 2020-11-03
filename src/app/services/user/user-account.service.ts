1df50a2ebb0a9ad2ffcf3cfe26dfa1b866e12cd5
ashar <ashara.jalpa@gmail.com>
2020-11-03 09:46:00 -0800

    Revert "Feature: User delete"
    This reverts commit f8e40afa879e8d7a3ed47b3bcbe98988e84d9746.

---
+++ src/app/services/user/user-account.service.ts
@@ -0,0 +1,59 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {UserDetails} from '../../models/user-details';
+import {environment} from '../../../environments/environment';
+import {BehaviorSubject, Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserAccountService {
+
+  temp: UserDetails;
+  private user = new BehaviorSubject<UserDetails>(this.temp);
+  currUser = this.user.asObservable();
+
+
+  constructor(public http: HttpClient) { }
+
+  setUserDetails(user) {
+    /*const u = {
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email,
+      phone: ' '
+    };*/
+    const url = environment.api + '/user';
+    return this.http.post(url, user);
+  }
+
+  getUserWithEmail(emailId: string) {
+    const u = {
+      email: emailId
+    };
+    const url = environment.api + '/user';
+    return this.http.post(url, u);
+  }
+
+  getUserById(id: number): Observable<any> {
+    const url = environment.api + '/user/' + id;
+    return this.http.get(url);
+  }
+
+  setCurrUserDetails(user: UserDetails) {
+    this.user.next(user);
+    let usr;
+    this.currUser.subscribe(res =>  usr = res);
+    localStorage.setItem('currUser', JSON.stringify(usr));
+  }
+
+  getCurrUserDetails() {
+    return JSON.parse(localStorage.getItem('currUser'));
+  }
+
+  updateUserInfo(id, data) {
+    const url = environment.api + '/user/' + id;
+    return this.http.put(url, data);
+
+  }
+}
